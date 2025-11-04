3a3920a6a89fc75e26492d014fbe8a0f412606eb
murodov109 <akabekov77@gmail.com>
2025-11-04 11:28:59 +0500

    Create battle.py

---
+++ battle.py
@@ -0,0 +1,7 @@
+def battle_konkurs(bot, call):
+    bot.send_message(
+        call.message.chat.id,
+        "⚔️ Batl konkursi boshlandi!\n"
+        "Ishtirokchilar raqobatlashadi, eng ko‘p ovoz olgan g‘olib bo‘ladi!\n"
+        "Bot: @unversal_konkurs_bot"
+    )
